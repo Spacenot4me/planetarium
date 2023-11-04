91219328dd98210ac5fa5cc08c1bc2f902aa6275
clorkapunk <146718898+clorkapunk@users.noreply.github.com>
2023-11-04 15:24:24 +0600

    -

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import planet1 from "./images/planet1.png"
+import planet2 from "./images/planet2.png"
+import planet3 from "./images/planet3.png"
+import {Carousel, Image} from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Carousel variant="dark" fade={true} slide={true} interval={1}>
+            <Carousel.Item>
+                <div className="carousel-planet-item">
+                    <Image src={planet1} className="planet-holder" />
+
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div className="carousel-planet-item">
+                    <Image src={planet2} className="planet-holder" />
+
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <div className="carousel-planet-item">
+                <Image src={planet3} className="planet-holder" />
+
+                </div>
+            </Carousel.Item>
+        </Carousel>
+
+    );
 }
 
 export default App;
