b364e94067a1b5433d9d1bb376b79c596fb1c8f6
clorkapunk <146718898+clorkapunk@users.noreply.github.com>
2023-11-05 12:18:12 +0600

    -

--- src/App.js
+++ src/App.js
@@ -20,30 +20,14 @@ function App() {
     
     const refContainer = useRef(null);
     useEffect(() => {
+        // renderer
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.domElement);
 
+
+        // scene
         const scene = new THREE.Scene();
-
-
-        const camera = new THREE.PerspectiveCamera(
-            45,
-            window.innerWidth / window.innerHeight,
-            0.1,
-            1500
-        );
-
-        const orbit = new OrbitControls(camera, renderer.domElement);
-        orbit.minPolarAngle = Math.PI * 0.3;
-        orbit.maxPolarAngle =  Math.PI * 0.5;
-
-        camera.position.set(-90, 100, 140);
-        orbit.update();
-
-        const ambientLight = new THREE.AmbientLight(0x333333);
-        scene.add(ambientLight);
-
         const cubeTextureLoader = new THREE.CubeTextureLoader();
         scene.background = cubeTextureLoader.load([
             starsTexture,
@@ -54,8 +38,39 @@ function App() {
             starsTexture
         ]);
 
-        const textureLoader = new THREE.TextureLoader();
 
+        //camera
+        const camera = new THREE.PerspectiveCamera(
+            45,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1500
+        );
+        camera.position.set(-90, 100, 140);
+
+
+        //orbit controls
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.minPolarAngle = Math.PI * 0.3;
+        controls.maxPolarAngle =  Math.PI * 0.5;
+        controls.addEventListener('start', () => {
+            renderer.setAnimationLoop(animateUserControls)
+        });
+        controls.addEventListener('end', () => {
+            renderer.setAnimationLoop(animateDef)
+        });
+        controls.update();
+
+
+        //light
+        const ambientLight = new THREE.AmbientLight(0x333333);
+        scene.add(ambientLight);
+        const pointLight = new THREE.PointLight(0xFFFFFF, 3000, 300);
+        scene.add(pointLight);
+
+
+        // sun mesh
+        const textureLoader = new THREE.TextureLoader();
         const sunGeo = new THREE.SphereGeometry(16, 30, 30);
         const sunMat = new THREE.MeshBasicMaterial({
             map: textureLoader.load(sunTexture)
@@ -63,6 +78,8 @@ function App() {
         const sun = new THREE.Mesh(sunGeo, sunMat);
         scene.add(sun);
 
+
+        // planets create
         function createPlanet(size, texture, position, ring) {
             const geo = new THREE.SphereGeometry(size, 30, 30);
             const mat = new THREE.MeshStandardMaterial({
@@ -108,9 +125,8 @@ function App() {
         const neptune = createPlanet(7, neptuneTexture, 200);
         const pluto = createPlanet(2.8, plutoTexture, 216);
 
-        const pointLight = new THREE.PointLight(0xFFFFFF, 3000, 300);
-        scene.add(pointLight);
 
+        // planets properties
         mercury.mesh.material.transparent = true;
         venus.mesh.material.transparent = true;
         earth.mesh.material.transparent = true;
@@ -121,7 +137,51 @@ function App() {
         neptune.mesh.material.transparent = true;
         pluto.mesh.material.transparent = true;
 
-        function animate() {
+
+        // detect intersection
+        let mousePointer = new THREE.Vector2();
+        const raycaster = new THREE.Raycaster();
+        function getMouseVector2(event, window){
+            let mousePointer = new THREE.Vector2()
+
+            mousePointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+            mousePointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+            return mousePointer;
+        }
+        function checkRayIntersections(mousePointer, camera, raycaster, scene, getFirstValue) {
+            raycaster.setFromCamera(mousePointer, camera);
+
+            let intersections = raycaster.intersectObjects(scene.children, true);
+
+            intersections = getFirstValue ? intersections[0] : intersections;
+
+            return intersections;
+        }
+
+        document.addEventListener('mousemove', onMouseMove, false);
+        function onMouseMove(event) {
+            const planets = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto'];
+
+            mousePointer = getMouseVector2(event, window);
+
+            const getFirstValue = true;
+
+            const intersections = checkRayIntersections(mousePointer, camera, raycaster, scene, getFirstValue);
+            if(intersections !== undefined){
+                for (const planet of planets) {
+                    
+                }
+                eval('earth').mesh.material.opacity = 0.1;
+            }
+            // const cardList = getCardObjects(intersections);
+            // flipCards(cardList, flippedCardsList);
+        }
+
+
+
+
+        function animateDef() {
             //Self-rotation
             sun.rotateY(0.004);
             mercury.mesh.rotateY(0.004);
@@ -148,7 +208,26 @@ function App() {
             renderer.render(scene, camera);
         }
 
-        renderer.setAnimationLoop(animate);
+        function animateUserControls() {
+            //Self-rotation
+            sun.rotateY(0.004);
+            mercury.mesh.rotateY(0.004);
+            venus.mesh.rotateY(0.002);
+            earth.mesh.rotateY(0.02);
+            mars.mesh.rotateY(0.018);
+            jupiter.mesh.rotateY(0.04);
+            saturn.mesh.rotateY(0.038);
+            uranus.mesh.rotateY(0.03);
+            neptune.mesh.rotateY(0.032);
+            pluto.mesh.rotateY(0.008);
+
+            renderer.render(scene, camera);
+        }
+
+
+
+        renderer.setAnimationLoop(animateDef);
+
 
         window.addEventListener('resize', function() {
             camera.aspect = window.innerWidth / window.innerHeight;
